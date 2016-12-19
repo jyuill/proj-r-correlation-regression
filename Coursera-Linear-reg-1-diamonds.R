0b97cc858788b0a2566266ece3dfe68e4d1b6519
jyuill <john@fig4.com>
2016-12-18 16:05:02 -0800

    edits

--- Coursera-Linear-reg-1-diamonds.R
+++ Coursera-Linear-reg-1-diamonds.R
@@ -10,14 +10,13 @@ ggplot(diamond, aes(x=carat,y=price))+
   xlab("Carats")+
   ylab("Price")+
   geom_point()+
-  geom_smooth(method="lm", color="green")
+  geom_smooth(method="lm", color="purple")
 
 fit <- lm(price ~ carat, data=diamond)
 coef(fit)
 
 summary(fit)
 
+## mean-center carat values by subtracting mean from each value to reset the intercept (x=0) to the mean
 fit2 <- lm(price ~ I(carat-mean(carat)), data=diamond)
 summary(fit2)
-
-## this
