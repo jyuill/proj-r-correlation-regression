330cce10d75a9c23bfa6af45f1d0b7b65b2d1b83
jyuill <john@fig4.com>
2017-05-21 16:43:10 -0700

    logistic regression on mtcars using two response variables - nice!

--- iris-example-multi.R
+++ iris-example-multi.R
@@ -52,7 +52,65 @@ ggplot(ir2, aes(x=Species,y=Petal.Width))+geom_boxplot()
 
 ## WHAT I WANT TO KNOW IS:
 ## what are the techniques for determining more certainly where to draw the lines
-## - logistic regression on petal.width ?
+## - logistic regression using species identifier as dependent variable ?
 ##    - test with both sepal width and pedal width
 ## - clustering to establish break-points?
 
+## - logistic regression on sepal.width
+logr1 <- glm(formula=spec~Sepal.Width, family=binomial, data=ir3)
+summary(logr1)
+## get Sepal.Width coefficient
+logr1$coefficients[2]
+## get exp of coefficient to determine % increase chance of identifying species with increase in sepal.width
+## (not sure if that is at all correct)
+exp(logr1$coefficient[2])-1
+
+## generate plot with fitted values - note how switches over around 3
+plot(ir3$Sepal.Width,fitted.values(logr1))
+## add observed values in red
+points(ir3$Sepal.Width,ir3$spec,col="red")
+
+## get prediction based on sepal width
+nd <- data.frame(Sepal.Width=3.5)
+## at 3.5 there is 94% chance that dependent variable is 1 (versicolor)
+predict(logr1,nd, type='response')
+
+## logistic regression on pedal.width
+## - model doesn't work - 'algorithm did not converge'
+## - because there is no overlap?
+logr2 <- glm(formula=ir3$spec~ir3$Petal.Width, family=binomial)
+summary(logr2)
+
+## generate plot with fitted values - note how switches over around 3
+plot(ir3$Petal.Width,fitted.values(logr2))
+## add observed values in red
+points(ir3$Petal.Width,ir3$spec,col="red")
+
+## get prediction based on petal width
+
+### LOGISTIC REGRESSION WITH MTCARS
+## can we determine if a car likely to have V engine or not based on car weight and engine displacement
+## 
+head(mtcars)
+summary(mtcars) ## can see that vs is either 0 or 1
+## dependent variable is categorical, independent (explanatory) variables are continuous
+## create a model
+model <- glm(formula=vs~wt+disp, data=mtcars, family='binomial')
+## check out the model
+model
+## get summary
+summary(model)
+## notes:
+## - deviance: lower better
+##   - Null deviance: without explanatory variables, null dev is 44
+##   - Residual deviance: with explanatory variables added, deviance drops to 21.4 - sign of progress
+## - AIC: lower better
+##   - used to compare models (not useful here, since not comparing)
+## - coefficients
+##   - wt and disp for explanatory variables
+##   - logit coefficients -> represent e to the power of the coefficient
+##   - wt: adding 1 unit of weight means log of odds of car being a '1' in response variable increase by e to 1.63
+
+## make prediction based on wt and displacement
+newdata<- data.frame(wt=2.1,disp=180)
+predict(model, newdata, type='response')
